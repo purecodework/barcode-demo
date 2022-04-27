203680e210e591b369538ed40209413ac54c50f8
Kent <kent@Kents-MacBook-Pro.local>
2022-04-28 09:56:33 +1000

    remove size limit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return(
     <div className='flex flex-col items-center justify-center'>
               <h1>Barcode Scanner demo</h1>
-              <div className='text-center h-1/2 w-1/2'>
+              <div className='text-center'>
               <Html5QrcodePlugin 
                 fps={10}
                 qrbox={250}
