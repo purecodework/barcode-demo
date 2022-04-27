4da51f4e5f2ab772533229e8d7a34a26f7085fb9
Kent <kent@Kents-MacBook-Pro.local>
2022-04-28 09:31:14 +1000

    init

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+import React from 'react';
+import Html5QrcodePlugin from './Html5QrcodeScannerPlugin'
+import { useState } from 'react';
+
+const App = () => {
+  const [scanResult, setScanResult] = useState()
+  const onNewScanResult = (decodedText, decodedResult) =>{
+      setScanResult(decodedText)
+  }
+  return(
+    <div className='flex flex-col items-center justify-center'>
+              <h1>Barcode Scanner</h1>
+              <div className='text-center h-1/2 w-1/2'>
+              <Html5QrcodePlugin 
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}/>
+                 <p>Bar code: {scanResult}</p>
+              </div>
+             
+               
+    </div>
+  )
+   
+}
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//       super(props);
+//       // This binding is necessary to make `this` work in the callback.
+//       this.onNewScanResult = this.onNewScanResult.bind(this);
+//   }
+
+//   render() {
+//       return (<div>
+//           <h1>Html5Qrcode React example!</h1>
+//           <Html5QrcodePlugin 
+//               fps={10}
+//               qrbox={250}
+//               disableFlip={false}
+//               qrCodeSuccessCallback={this.onNewScanResult}/>
+//       </div>);
+//   }
+
+//   onNewScanResult(decodedText, decodedResult) {
+//       // Handle the result here.
+//   }
+// };
+
+export default App
